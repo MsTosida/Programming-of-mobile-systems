060dcdcc0346609a98f17087d1b562343e571835
tosida <you.golubitskaya@yandex.ru>
2024-03-15 10:40:25 +0300

    add lab 4-5

---
+++ labb4_5/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:labb4_5/pages/datail_page.dart';
+import 'package:labb4_5/pages/main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(),
+      //DetailPage() ,
+    );
+  }
+}
